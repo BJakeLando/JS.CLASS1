ffc6196e6820f674da9c344d3f65d67799fff2bf
BJakeLando <bjlando@gmail.com>
2022-08-22 17:25:31 -0700

    Assignment 1
    Netflix movie list with actors

---
+++ script.js
@@ -0,0 +1,35 @@
+let userName ="Netflix";
+let yearCreated=2022; //defining the var age
+let movieOne="Grayman"; // does not change the value
+let movieDirector="Joe Russo";
+let movieWriter="Ryan Gosling";
+let actorOne="Ryan Gosling";
+let actorTwo="Chris Evans";
+let actorThree="Ana de Armas";
+let actorFour="Dhanush";
+let actorFive="Julia Butters"
+let actorSix="Jessia Henwick";
+let actorSeven="Wagner Moura";
+let actorEight="Billy Bob Thornton";
+
+
+//document.write("<p> Name: " + usernName + "</p><p>Age: " + age + "</p>")
+
+document.write(`
+<p>Company Name: ${userName}</p>
+<p>Movie 1: ${movieOne}</p>
+<p>YearCreated: $${2022}</p> 
+<p>Movie Director: ${movieDirector}</p>
+<p>Movie Writer: ${movieDirector}</p>
+<p>Movie Actor: ${actorOne}</p>
+<p>Movie Actor: ${actorTwo}</p>
+<p>Movie Actor: ${actorThree}</p>
+<p>Movie Actor: ${actorFour}</p>
+<p>Movie Actor: ${actorFive}</p>
+<p>Movie Actor: ${actorSix}</p>
+<p>Movie Actor: ${actorSeven}</p>
+<p>Movie Actor: ${actorEight}</p>
+
+`);
+
+console.log()
